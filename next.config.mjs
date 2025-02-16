7de90da9a52aa374c59a83263d5323c679fb2366
Raman Shinde <raman.shinde15@gmail.com>
2025-02-16 21:00:18 +0530

    Intial Commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',  // Required for GitHub Pages
+    basePath: '/ramashin',  // Set this to your GitHub repository name
+    assetPrefix: '/ramashin/', // Ensures assets load correctly
+  };
 
 export default nextConfig;
